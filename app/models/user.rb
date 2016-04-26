28aa52be4fb94bbb3b768e2882b9c77c3f3a003c
mikeachour17 <mikachour@gmail.com>
2016-04-26 16:51:16 -0700

    add pins and users association

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
                      :on => :create,
                      :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
 
+  has_many :pins
+
 end
